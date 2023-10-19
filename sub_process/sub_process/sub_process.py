d2e8e3f6281674f75931d859833070c14fb2ff87
Rojas Lobo <sanie.s.rojas.lobo@accenture.com>
2023-10-19 01:29:52 -0300

    improving sub_process code

--- sub_process/sub_process/sub_process.py
+++ sub_process/sub_process/sub_process.py
@@ -1,32 +1,29 @@
 """Analyzing text retrieved per subject"""
-import pandas as pd
 import re
-
+import json
+import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import pos_tag, ne_chunk
 
-nltk.download(['stopwords', 'vader_lexicon', 'punkt']) 
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('x')
+nltk.download(['stopwords', 'vader_lexicon', 'punkt'], quiet=True) 
+nltk.download('maxent_ne_chunker', quiet=True)
+nltk.download('words', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
 
 
-#variables setting - importing from local 
 def file_to_df(user_folder, user_file):
+    """ Process the file into a pandas dataframe."""
     user_folder = input("Please introduce URL to working folder: ->  ")
     user_file = input("Please enter file name: ->  ")
     file_pointer = f"{user_folder}{user_file}"
     df = pd.read_csv(file_pointer)
     return df
 
-
-#define function for cleanse of the text 
 def clean_text(text):
-    # Use regular expressions to keep only letters and whitespace
+    """ Clean text."""
     my_stopwords = nltk.corpus.stopwords.words("english")
     cleaned_text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
     words = nltk.word_tokenize(cleaned_text)
@@ -34,10 +31,12 @@ def clean_text(text):
     return words_nstw
 
 def get_headlines_df(df):
+    """ Get headlines."""
     headlines = df.drop(columns=['desc','site','link','img','media','2023-10-17 23:17:49.446507'], axis=1)
     return headlines
 
 def generate_txt(df, filename):
+    """ Generates .txt file for furhter analysis."""
     filename = input("Please enter file name expected: ->  ")
     # Extract the specified column as a Pandas Series & Save the column data as text in a .txt file
     column_data = df["title"]
@@ -45,18 +44,18 @@ def generate_txt(df, filename):
     return print("Saved succesfully. The titles have been saved as text in {filename}'")
 
 def get_scores(df):
+    """ Get scores."""
     scores = []
+    analyzer = SentimentIntensityAnalyzer()
     for i in range(len(df)):
         tokens = df["tokens"][i]
         sentiment_score = analyzer.polarity_scores(' '.join(tokens))['compound']
         scores.append(sentiment_score)
-        i + 1
-
     df["score"] = scores
-
     return df
 
 def extract_named_entities(text):
+    """ Get entities."""
     cleaned_text = re.sub(r'[^a-zA-Z\s]', '', text).lower()
     words = nltk.word_tokenize(cleaned_text)
     pos_tags = nltk.pos_tag(words)
@@ -65,6 +64,7 @@ def extract_named_entities(text):
 
 #codigo a auditar #!!!!!!!!!!!!!!!!!!!!!!!
 def extract_entities(text_file):
+    """ extract main entities."""
     with open(text_file, 'r') as f:
         text = f.read()
     entities = {}
@@ -77,14 +77,16 @@ def extract_entities(text_file):
 #------
 
 if __name__ == "__main__":
-    working_directory = "/Users/sanie.s.rojas.lobo/Desktop/ITBA Bucket/subject_screener/file_store_search/"
-    file_name = "news_data_Israel_1697595469.csv"
-    #import csv
-    df = file_to_df(working_directory, file_name)
+
+    user_folder = "hi"
+    user_file = "hi"
+
+    #import file
+    dfn = file_to_df(user_folder, user_file)
     #transform to optimize for semantic analysis in various forms
-    df['tokens'] = df['title'].apply(clean_text)
+    dfn['tokens'] = dfn['title'].apply(clean_text)
     #simplify for analysis
-    headlines = get_headlines_df(df)
+    headlines = get_headlines_df(dfn)
     #obtain sentiment score
     get_scores(headlines)
     #save results by date to csv - txt
@@ -93,13 +95,14 @@ if __name__ == "__main__":
     #obtain main entities involved & save top 10 to subject by date
     headlines["named_entities"] = headlines["title"].apply(extract_named_entities)
     #save to CSV
-    output_csv = "processed news" #!!!!!!!!!!!!!!!!!!!!!!!
+    output_csv = "processed news" #!!!!!!!!!!!!!!!!!!!!!!
     headlines.to_csv(output_csv, header=False, index=False, sep='\t') 
     #generate main entities & sort them 
-    entities = extract_entities(filename)
+    entities = extract_entities("israel_output.txt")
     sorted_entities = dict(sorted(entities.items(), key=lambda item: item[1], reverse=True))
     #also save to csv
     output_csv_2 = "main_entities"
-    sorted_entities.to_csv(output_csv_2, header=False, index=False, sep='\t') 
 
+    with open("sorted_entities.json", "w") as file:
+        json.dump(sorted_entities, file)
     print("Sub_process complete")
